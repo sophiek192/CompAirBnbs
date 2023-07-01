68a32b6390f0ad3447a119f15f4df14f8e111b8f
Sophie Khov <z5424724@ad.unsw.edu.au>
2023-07-01 14:02:36 +1000

    dataStore.js created to initialise the dataStore

---
+++ backend/dataStore.js
@@ -0,0 +1,29 @@
+import fs from 'fs';
+
+let dataStore = {
+    users: [],
+    trip: [],
+}
+
+
+// reads from dataStore file
+const save = () => {
+    const jsonstr = JSON.stringify(data);
+    fs.writeFileSync('./database.json', jsonstr);
+  };
+  
+  // Use getData() to access the data
+  function getData() {
+    return dataStore;
+  }
+  
+  // Use set(newData) to pass in the entire data object, with modifications made
+  // - Only needs to be used if you replace the data store entirely
+  // - Javascript uses pass-by-reference for objects... read more here: https://stackoverflow.com/questions/13104494/does-javascript-pass-by-reference
+  // Hint: this function might be useful to edit in iteration 2
+  function setData(dataStore) {
+    data = dataStore;
+    save();
+  }
+
+export { getData, setData };
